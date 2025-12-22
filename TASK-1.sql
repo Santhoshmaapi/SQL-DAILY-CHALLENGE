8532b7ecf9baf211cd7c8b8084f84276f0b5a61a
Santhosh G <145673304+Santhoshmaapi@users.noreply.github.com>
2025-12-22 17:00:34 +0530

    Add files via upload

---
+++ TASK-1.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE City_Hospital;
+USE City_Hospital;
+
+CREATE TABLE Patients(
+	Patient_ID INT PRIMARY KEY,
+    Patient_Name VARCHAR(50),
+    Age INT,
+    Gender ENUM('M','F'),
+    Admission_Date DATE DEFAULT (CURRENT_DATE)
+);
+
+SELECT * FROM Patients;
